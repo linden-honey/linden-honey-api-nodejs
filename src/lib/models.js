79bfe22e7c354d9c9eaf23009ac4b8c5222e9186
Alexander Babai <aliaksandr.babai@gmail.com>
2017-01-16 21:00:37 +0300

    refactor(models): use exports instead of module.*

--- src/lib/models.js
+++ src/lib/models.js
@@ -1,17 +1,17 @@
-module.exports.Quote = class Quote {
+exports.Quote = class Quote {
     constructor(phrase) {
         this.phrase = phrase
     }
 }
 
-module.exports.Song = class Song {
+exports.Song = class Song {
     constructor(title, id) {
         this.title = title
         this.id = id
     }
 }
 
-module.exports.SongMeta = class SongMeta {
+exports.SongMeta = class SongMeta {
     constructor(title, author, album, text) {
         this.title = title
         this.author = author
