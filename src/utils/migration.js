5764f7ea322d954eca6059ee480ae24c649813ca
Alexander Babai <aliaksandr.babai@gmail.com>
2017-02-18 18:02:09 +0300

    refactor(db-migration): log init and finish states

--- src/utils/migration.js
+++ src/utils/migration.js
@@ -2,10 +2,10 @@ const fetch = require('node-fetch')
 const Song = require('../models/song')
 
 exports.initData = url => {
+    console.log('Data migration started...')
     fetch(url)
       .then(res => res.json())
-      .then(json => {
-          return Song.insertMany(json)
-      })
+      .then(json => Song.insertMany(json))
+      .then(() => console.log('Data migration succesfully finished!'))
       .catch(err => console.error('Data migration error:\n', err))
 }
