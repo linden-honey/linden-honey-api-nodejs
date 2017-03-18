b7516fade9f1ec052a9faa4dbb336772b0e05685
Alexander Babai <aliaksandr.babai@gmail.com>
2017-03-19 00:56:09 +0300

    feat(koa): update dependencies & remove docs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
-const Router = require('koa-router')
-const convert = require('koa-convert')
-const docs = require('koa-docs')
+const Router = require('koa-joi-router')
 
 const db = require('./utils/db')
-const docsHelper = require('./utils/docs-helper')
 const config = require('./utils/config')
 const constants = require('./utils/constants/path-constants')
 const rootController = require('./controllers/root-controller')
@@ -39,31 +36,10 @@ quotesRouter.get(`${constants.API_QUOTES}/:quoteId`, quoteController.getQuoteByI
 
 server.name = config.get('app:name')
 server.use(logger())
-server.use(rootRouter.routes())
-server.use(songsRouter.routes())
-server.use(versesRouter.routes())
-server.use(quotesRouter.routes())
-
-server.use(convert(docs.get('/docs', {
-    title: config.get('docs:title'),
-    version: config.get('app:version'),
-    theme: config.get('docs:theme'),
-    routeHandlers: config.get('docs:routeHandlers'),
-    groups: [
-        {
-            groupName: config.get('docs:groups:songs:title'),
-            routes: docsHelper.getRoutes(songsRouter.stack)
-        },
-        {
-            groupName: config.get('docs:groups:verses:title'),
-            routes: docsHelper.getRoutes(versesRouter.stack)
-        },
-        {
-            groupName: config.get('docs:groups:quotes:title'),
-            routes: docsHelper.getRoutes(quotesRouter.stack)
-        }
-    ]
-})))
+server.use(rootRouter.middleware())
+server.use(songsRouter.middleware())
+server.use(versesRouter.middleware())
+server.use(quotesRouter.middleware())
 
 server.listen(process.env.PORT || config.get('app:port') || 8080, () => {
     db.connect(config.get('db:config'))
