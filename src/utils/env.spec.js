c1f06fbd59c2945d5c29f8dc3e4ca79c47f19919
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-15 14:38:23 +0300

    test(env): change random method

--- src/utils/env.spec.js
+++ src/utils/env.spec.js
@@ -6,13 +6,13 @@ const { getEnv } = require('./env')
 describe('Env module', () => {
     describe('#getEnv(key, defaultValue?)', () => {
         it('Should return default value', () => {
-            const key = `KEY_${new Date().getMilliseconds()}`
+            const key = `KEY_${Math.random().toString(36)}`
             const defaultValue = 'defaultValue'
             const result = getEnv(key, defaultValue)
             expect(result).is.equal(defaultValue)
         })
         it('Should return a value', () => {
-            const key = `KEY_${new Date().getMilliseconds()}`
+            const key = `KEY_${Math.random().toString(36)}`
             const value = 'value'
             process.env[key] = value
             const result = getEnv(key)
@@ -22,8 +22,7 @@ describe('Env module', () => {
             expect(() => getEnv()).to.throw("Missing 'key' argument!")
         })
         it('Should throw an Error - required key', () => {
-            const key = `KEY_${new Date().getMilliseconds()}`
-            delete process.env[key]
+            const key = `KEY_${Math.random().toString(36)}`
             expect(() => getEnv(key)).to.throw(`"${key}" is required!`)
         })
     })
